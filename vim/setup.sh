66d3338a36b7cf0838eb6a7bb52b37f2d85220f6
Gustavo Fonseca <gustavo@gfonseca.net>
2014-06-14 10:35:15 -0300

    Correção: A flag -d deve ser usada para testar a existência de diretórios.

--- vim/setup.sh
+++ vim/setup.sh
@@ -8,7 +8,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # ------------------------------
 # Backup existing configuration.
 # ------------------------------
-if [ -f ~/.vim ]
+if [ -d ~/.vim ]
 then
     mv ~/.vim ~/.vim.backup
 fi
